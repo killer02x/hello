06326a815b9fc2fa9c6c35a16d7f4612a711e4ef
Azamat21 <bagdat.azamatt@gmail.com>
2023-09-01 16:53:15 +0600

    sw

--- src/App.js
+++ src/App.js
@@ -22,4 +22,19 @@ function App() {
   );
 }
 
-export default App;
+function MyButton() {
+  return (
+    <button>
+      Button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
